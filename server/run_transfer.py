13fad4adb75de73a2ecadda526c228eb54617773
Benny Khoo <benny.khoo@yahoo.com>
2024-08-26 12:03:08 +0800

    Removing chromedriver_binary.

--- server/run_transfer.py
+++ server/run_transfer.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-import chromedriver_binary
+# import chromedriver_binary
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
